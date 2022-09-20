5235b090bbc08425184f6a1845a6f3bb5aed8492
Jr Bytes <itamazor@gmail.com>
2022-09-20 19:17:30 -0300

    added createdeliveryman usecase

---
+++ src/modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express'
+import { CreateDeliverymanUseCase } from './CreateDeliverymanUseCase'
+
+export class CreateDeliverymanController {
+  async handle(request: Request, response: Response) {
+    const { username, password } = request.body
+
+    const createDeliveryUseCase = new CreateDeliverymanUseCase()
+
+    const delivery = await createDeliveryUseCase.execute({
+      username,
+      password
+    })
+
+    return response.status(201).json(delivery)
+  }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,16 @@
 import { Router } from "express";
 import { AuthenticateClientController } from "./modules/accounts/authenticateUser/AuthenticateUserController";
 import { CreateClientController } from "./modules/clients/useCases/createClient/CreateClientController";
+import { CreateDeliverymanController } from "./modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController";
 
 const routes = Router()
 
 const createClientController = new CreateClientController()
 const authenticateClientController = new AuthenticateClientController()
+const createDeliverymanController = new CreateDeliverymanController()
 
 routes.post('/authenticate', authenticateClientController.handle)
 routes.post('/clients', createClientController.handle)
+routes.post('/deliveryman', createDeliverymanController.handle)
 
 export { routes }
